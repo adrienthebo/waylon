5e2e5eb0ca4ddd1e0823be132538b8f84277ae5c
Roger Ignazio <me@rogerignazio.com>
2014-04-28 22:22:57 -0700

    implement job filter

--- waylon.rb
+++ waylon.rb
@@ -12,11 +12,14 @@ class Waylon < Sinatra::Application
 
     # For each Jenkins instance in "jobs", connect to the server,
     # and get the status of the jobs specified in the config.
+    filter            = []
     failed_builds     = []
     successful_builds = []
 
     config['jobs'].each do |hash|
       hash.keys.each do |server|
+        filter += hash[server] # build the job filter
+
         client = JenkinsApi::Client.new(:server_url => server)
 
         # list_by_status() gets an alphanumeric list of jobs based on the
@@ -26,8 +29,22 @@ class Waylon < Sinatra::Application
         # alphanumeric order. If you want them to be displayed differently
         # (e.g. last-run at the top), take a look at list_details(job_name).
         # That approach is slightly more complicated though.
-        failed_builds     += client.job.list_by_status('failure')
-        successful_builds += client.job.list_by_status('success')
+
+        # Get the list of failed jobs. If the job is in the job filter,
+        # append it to the array that we'll display in the dashboard.
+        client.job.list_by_status('failure').each do |failed_build|
+          if(filter.include?(failed_build)) then
+             failed_builds << failed_build
+          end
+        end
+
+        # Get the list of successful jobs. If the job is in the job filter,
+        # append it to the array that we'll display in the dashboard.
+        client.job.list_by_status('success').each do |successful_build|
+          if(filter.include?(successful_build)) then
+             successful_builds << successful_build
+          end
+        end
       end
     end
 
