4bc737a65f66a44f8aeebc9a51b27c6fad3185ee
Roger Ignazio <me@rogerignazio.com>
2014-05-04 16:01:44 -0700

    refactor sinatra views
    Refactor Sinatra views to make use of nested ERB templates (single base
    template, followed by per-page content templates), and to use instance
    variables to cleanup the mess that :locals => { ... } turned out to be.

--- waylon.rb
+++ waylon.rb
@@ -5,7 +5,6 @@ require 'yaml'
 
 class Waylon < Sinatra::Application
   helpers do
-
     # escape() sanitizes text for use as a URL
     def escape(text)
       Rack::Utils.escape(text)
@@ -44,34 +43,37 @@ class Waylon < Sinatra::Application
         }
       end
     end
-
   end
 
   # Landing page (index.html)
   # Print a list of views available on this Waylon instance.
   get '/' do
-    views = get_views()
-    erb :index, :locals => { :views => views }
+    @this_view = 'index'
+    @nirvana   = false
+
+    erb :base do
+      erb :index
+    end
   end
 
   # Individual views (view/foo)
   # When navigating to 'view/foo', queries waylon_config.yml for
   # that view's config (servers to connect to, and jobs to display).
   get '/view/:name' do
-    this_view = Rack::Utils.unescape(params[:name])
+    @this_view = Rack::Utils.unescape(params[:name])
     config = YAML.load_file(File.join(File.dirname(__FILE__), 'waylon_config.yml'))
 
     # Refresh the page every `refresh_interval` seconds.
-    refresh_interval = config['config'][0]['refresh_interval'].to_s
+    @refresh_interval = config['config'][0]['refresh_interval'].to_s
 
     # For each Jenkins instance in "jobs", connect to the server,
     # and get the status of the jobs specified in the config.
-    errors          = []
-    failed_jobs     = []
-    building_jobs   = []
-    successful_jobs = []
+    @errors          = []
+    @failed_jobs     = []
+    @building_jobs   = []
+    @successful_jobs = []
 
-    config['views'].select { |h| h[this_view] }[0].each do |_, servers|
+    config['views'].select { |h| h[@this_view] }[0].each do |_, servers|
       servers.each do |hash|
         hash.keys.each do |server|
           client = JenkinsApi::Client.new(:server_url => server)
@@ -79,7 +81,7 @@ class Waylon < Sinatra::Application
           begin
             client.get_root  # Attempt a connection to `server`
           rescue SocketError
-            errors << "Unable to connect to server: #{server}"
+            @errors << "Unable to connect to server: #{server}"
             next
           end
 
@@ -87,11 +89,11 @@ class Waylon < Sinatra::Application
             job_details = client.job.list_details(job)
             case client.job.color_to_status(job_details['color'])
             when 'running'
-              building_jobs << job_details
+              @building_jobs << job_details
             when 'failure'
-              failed_jobs << job_details
+              @failed_jobs << job_details
             when 'success'
-              successful_jobs << job_details
+              @successful_jobs << job_details
             end
           end
         end
@@ -101,24 +103,15 @@ class Waylon < Sinatra::Application
     # If all jobs are green, display the image of the day. These images
     # are located in `public/img/` and follow the convention `imgNN.png`,
     # where NN is the day (number) of the week, starting with 0 (Sunday).
-    if(failed_jobs.empty? and errors.empty?) then
-      nirvana = true
+    if(@failed_jobs.empty? and @errors.empty?) then
+      @nirvana = true
       wday = Time.new.wday
-      nirvana_img = "../img/img0#{wday}.png"
+      @nirvana_img = "../img/img0#{wday}.png"
     end
 
-    erb :view,
-      :locals => {
-      :refresh_interval => refresh_interval,
-      :this_view        => this_view,
-      :views            => get_views(),
-      :nirvana          => nirvana,
-      :nirvana_img      => nirvana_img,
-      :errors           => errors,
-      :failed_jobs      => failed_jobs,
-      :building_jobs    => building_jobs,
-      :successful_jobs  => successful_jobs,
-    }
+    erb :base do
+      erb :view
+    end
   end
 end
 
