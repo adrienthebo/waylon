9347738c6e73c31844e1bc73eb43f2425e0fa6b0
Roger Ignazio <me@rogerignazio.com>
2014-05-05 22:14:15 -0700

    coding style clean-up

--- public/js/waylon.js
+++ public/js/waylon.js
@@ -11,7 +11,8 @@
 
 var waylon = {
     // Allow configuration to be passed-in from the erb template.
-    init: function(settings) {
+    init: function (settings) {
+        'use strict';
         waylon.config = { };
         $.extend(waylon.config, settings);
         $(document).ready(waylon.setup());
@@ -19,44 +20,53 @@ var waylon = {
 
     // Setup the Waylon routine once the document is ready.
     // Repeat every 'refresh_interval' seconds.
-    setup: function() {
-        refreshInterval = waylon.config.refresh_interval * 1000;
-        waylon.refreshRadiator()
+    setup: function () {
+        'use strict';
+        var refreshInterval = waylon.config.refresh_interval * 1000;
+        waylon.refreshRadiator();
         setInterval(waylon.refreshRadiator, refreshInterval);
     },
 
     // Return the image of the day for nirvana mode
-    imageOfTheDay: function() {
-        date = new Date();
-        day = date.getDay();
-        result = "/img/img0" + day + ".png";
+    imageOfTheDay: function () {
+        'use strict';
+        var date = new Date(),
+            day = date.getDay(),
+            result = "/img/img0" + day + ".png";
         return result;
     },
 
     // Nirvana mode enablement. Checks for the number of elements on the
     // page belonging to any of the classes listed in elems[]. If any are
     // found, returns false.
-    nirvanaCheck: function() {
-        elems = ['.building-job', '.failed-job', '.alert-danger'];
-        var count = 0;
+    nirvanaCheck: function () {
+        'use strict';
+        var elems = ['.building-job', '.failed-job', '.alert-danger'],
+            count = 0;
 
-        $.each(elems, function(i, elem) {
+        $.each(elems, function (i, elem) {
             count += $(elem).length;
         });
 
-        if(count == 0) { return true;  }
-        else           { return false; }
+        if (count === 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
     },
 
     // Enter nirvana mode
-    nirvanaBegin: function() {
+    nirvanaBegin: function () {
+        'use strict';
         $('body').addClass('nirvana');
-        $('body').css('background-image', 'url('+ waylon.imageOfTheDay() + ')');
+        $('body').css('background-image', 'url(' + waylon.imageOfTheDay() + ')');
         $('#radiator').hide();
     },
 
     // Exit nirvana mode
-    nirvanaEnd: function() {
+    nirvanaEnd: function () {
+        'use strict';
         $('body').removeClass('nirvana');
         $('body').css('background-image', 'none');
         $('#radiator').show();
@@ -64,8 +74,9 @@ var waylon = {
 
     // Poll '/view/:name/data' to fetch the latest data from Jenkins,
     // starting and ending Nirvana mode as needed.
-    refreshRadiator: function() {
-        $(document).ajaxSend(function() {
+    refreshRadiator: function () {
+        'use strict';
+        $(document).ajaxSend(function () {
             $('#loading').show();
         });
         $(document).ajaxComplete(function () {
@@ -76,12 +87,16 @@ var waylon = {
             $('[data-toggle="tooltip"]').tooltip({'placement': 'bottom'});
 
             // Nirvana mode
-            isNirvana = waylon.nirvanaCheck();
-            if(isNirvana) { waylon.nirvanaBegin(); }
-            else          { waylon.nirvanaEnd();   }
+            var isNirvana = waylon.nirvanaCheck();
+            if (isNirvana) {
+                waylon.nirvanaBegin();
+            }
+            else {
+                waylon.nirvanaEnd();
+            }
         });
 
         // Do it.
         $('#radiator').load(waylon.config.datasource);
-    },
+    }
 };
